adc6da51e0daf5c1d39c1f7594c04fbdc0432704
Vass Norbert <vassnorbert20@gmail.com>
2026-01-09 15:42:11 +0100

    Updating file-upload component

--- src/lib/components/file-upload.svelte
+++ src/lib/components/file-upload.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { PageState } from '$lib/utils/utils';
+	import { Upload } from '@lucide/svelte';
 	import { toast } from 'svelte-sonner';
 
 	type Props = {
@@ -8,24 +9,54 @@
 	};
 
 	let { pageState = $bindable(), uploadValidation }: Props = $props();
+	let isDragged = $state(false);
 
-	async function handleUpload(event: Event) {
-		event.preventDefault();
-
-		const input = event.target as HTMLInputElement;
-
-		if (!input.files) {
+	async function handleUpload(fileList: FileList | null) {
+		if (!fileList) {
 			toast.error('Error while trying to upload files');
 			return;
 		}
 
-		const inputFiles = Array.from(input.files);
+		const files = Array.from(fileList);
 
-		if (await uploadValidation(inputFiles)) {
-			pageState.files = inputFiles.map((file) => ({ input: file, status: 'idle' }));
+		if (await uploadValidation(files)) {
+			pageState.files = files.map((file) => ({ input: file, status: 'idle' }));
 		}
+	}
+
+	async function handleChange(event: Event) {
+		event.preventDefault();
+		isDragged = false;
+		await handleUpload((event.target as HTMLInputElement).files);
+	}
+
+	async function handleDrop(event: DragEvent) {
+		event.preventDefault();
+		isDragged = false;
+		if (event.dataTransfer?.files) {
+			await handleUpload(event.dataTransfer.files);
+		}
+	}
+
+	async function handleDragover(event: DragEvent) {
+		event.preventDefault();
+		isDragged = true;
 	}
 </script>
 
-<label for="file_upload" class="underline-offset-4 hover:underline">Click to upload files</label>
-<input onchange={handleUpload} id="file_upload" type="file" multiple class="hidden" />
+<label
+	ondrop={handleDrop}
+	ondragover={handleDragover}
+	ondragleave={() => (isDragged = false)}
+	for="file_upload"
+	class={[
+		'z-40 flex w-full max-w-3xl flex-col items-center justify-center rounded-lg border border-dashed py-28 hover:border-foreground',
+		isDragged ? 'border-foreground' : 'border-muted-foreground'
+	]}>
+	<Upload class="size-8 text-muted-foreground" />
+	<div class="py-4 text-lg font-medium">Upload Files</div>
+	<div class="text-sm text-muted-foreground">Drag and drop or click to upload</div>
+	<div class="text-sm text-muted-foreground">Accepts files up to 2.00GB in size.</div>
+</label>
+
+<input onchange={handleChange} id="file_upload" type="file" multiple class="hidden" />
